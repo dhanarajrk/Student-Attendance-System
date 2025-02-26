6a99b3184f8db8c47ddd9470014df16a3a6ad252
Dhanaraj Rk <dhanarajrk51@gmail.com>
2025-02-26 19:21:01 +0530

    Added GitHub Actions for backend testing

--- backend/tests/sample.test.js
+++
@@ -1,9 +0,0 @@
-import request from "supertest";
-import app from "../index.js"; // Ensure your Express app is exported
-
-describe("Sample Test", () => {
-  it("should return a 200 status", async () => {
-    const res = await request(app).get("/");
-    expect(res.statusCode).toBe(200);
-  });
-});
