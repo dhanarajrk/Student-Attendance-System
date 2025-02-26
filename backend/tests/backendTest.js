2e42569b4c8b782ece7244356a741b4620ad8379
Dhanaraj Rk <dhanarajrk51@gmail.com>
2025-02-26 19:35:32 +0530

    Added New CI Workflow test

--- backend/tests/backendTest.js
+++ backend/tests/backendTest.js
@@ -24,10 +24,7 @@ requiredEnvVars.forEach((envVar) => {
 // Test MongoDB Connection
 async function testMongoDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected Successfully!");
   } catch (error) {
     console.error("❌ MongoDB Connection Failed:", error.message);
